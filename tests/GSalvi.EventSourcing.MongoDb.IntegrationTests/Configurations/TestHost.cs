de6d7feb926404bb26944f4457226c8690dacc24
Guilherme Salvi <guilhermesalvi.dev@outlook.com>
2021-07-05 17:02:16 -0300

    Fix configurations directory

--- tests/GSalvi.EventSourcing.MongoDb.IntegrationTests/Configurations/TestHost.cs
+++ tests/GSalvi.EventSourcing.MongoDb.IntegrationTests/Configurations/TestHost.cs
@@ -14,7 +14,7 @@ namespace GSalvi.EventSourcing.MongoDb.IntegrationTests.Configurations
         {
             var configurationBuilder =
                 new ConfigurationBuilder()
-                    .SetBasePath($"{Directory.GetCurrentDirectory()}/Configurations")
+                    .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Configurations"))
                     .AddJsonFile("appsettings.json", true)
                     .Build();
 
